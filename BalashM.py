5e579bc4864889401c28f37d150bb9f53c9e0fc3
Demiko <demiko18@gmail.com>
2017-03-14 18:10:55 +0200

    added parameter to control computing of better results

--- BalashM.py
+++ BalashM.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
 
-def mod(c, a, b):
+def mod(c, a, b, do_iterations=True):
     def prio_mod(c, a, b):
         m = len(b)
         n = len(c)
@@ -10,10 +10,10 @@ def mod(c, a, b):
         p.sort(reverse=True)
         return [i for _, i in p]
 
-    return mod_balas(c, a, b, prio_mod)
+    return mod_balas(c, a, b, prio_mod, do_iterations)
 
 
-def greed(c, a, b):
+def greed(c, a, b, do_iterations=True):
     def prio_greed(c, a, b):
         m = len(b)
         n = len(c)
@@ -22,10 +22,10 @@ def greed(c, a, b):
         p.sort(reverse=True)
         return [i for _, i in p]
 
-    return mod_balas(c, a, b, prio_greed)
+    return mod_balas(c, a, b, prio_greed, do_iterations)
 
 
-def my(c, a, b):
+def my(c, a, b, do_iterations=True):
     def prio_my(c, a, b):
         m = len(b)
         n = len(c)
@@ -34,15 +34,16 @@ def my(c, a, b):
         p.sort(reverse=True)
         return [i for _, i in p]
 
-    return mod_balas(c, a, b, prio_my)
+    return mod_balas(c, a, b, prio_my, do_iterations)
 
 
-def mod_balas(c: list, a: list, b: list, prio_compute) -> (list, float, int):
+def mod_balas(c: list, a: list, b: list, prio_compute: callable, do_iterations: bool = True) -> (list, float, int):
     """
     :param c: list
     :param a: list
     :param b: list
-    :param prio_compute: function
+    :param prio_compute: callable
+    :param do_iterations: bool
     :return: (list, float, int)
     """
     m = len(b)
@@ -65,21 +66,22 @@ def mod_balas(c: list, a: list, b: list, prio_compute) -> (list, float, int):
     px = prio_compute(c, a, b)
     x = buildvar(px, a, b)
     z = sum(c[j] for j in range(n) if x[j] == 1)
-    pz = [j for j in range(n) if x[j] == 0]
     iters = 0
-    for k in pz:
-        iters += 1
-        zn = 0
-        xn = [0] * n
-        xn[k] = 1
-        bk = [(b[i] - a[i][k]) for i in range(m)]
-        if all(b[i] - a[i][j] < 0 for j in range(n) if j != k for i in range(m)):
-            continue
-        xn = buildvar(px, a, bk, xn)
-        zn = sum(c[j] * xn[j] for j in range(n))
-        if zn > z:
-            z = zn
-            x = xn
+    if do_iterations:
+        pz = [j for j in range(n) if x[j] == 0]
+        for k in pz:
+            iters += 1
+            zn = 0
+            xn = [0] * n
+            xn[k] = 1
+            bk = [(b[i] - a[i][k]) for i in range(m)]
+            if all(b[i] - a[i][j] < 0 for j in range(n) if j != k for i in range(m)):
+                continue
+            xn = buildvar(px, a, bk, xn)
+            zn = sum(c[j] * xn[j] for j in range(n))
+            if zn > z:
+                z = zn
+                x = xn
     return x, z, iters
 
 
