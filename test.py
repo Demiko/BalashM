ffebe33040f096479b04c158aa4c932c5d1f7007
Demiko <demiko18@gmail.com>
2017-03-14 19:23:56 +0200

    new test script

--- test.py
+++ test.py
@@ -1,34 +1,40 @@
 #!/usr/bin/env python3
 # coding=utf-8
 
-import cProfile
+from sys import stdout
 from random import randint as randi
-
+from BalashM import greed
 from BalashM import mod
+from Ant import ant
+from Balas import balas
 
-#  тестовая задача из статьи
-c = [4, 7, 2, 1, 3, 5]
-b = [14, 11]
-a = [
-    [2, 5, 1, 2, 3, 4],
-    [4, 3, 2, 4, 2, 3]
-    ]
-#  генератор тестовых задач
-n = 20
-m = 10
-c = [randi(1, 5) for i in range(n)]
-a = [[]] * m
-for i in range(len(a)):
-    a[i] = [randi(1,5) for i in range(n)]
-b = [randi(n, 3*n) for i in range(m)]
-x, z, iters = mod(c, a, b)
-cProfile.run('mod(c, a, b)', sort='tottime')
-print('C =', c)
-print('\nB =', b)
-print('\nA =')
-for i in range(len(a)):
-    print(a[i])
-print('\nX =', x)
-print('Z =', z)
-print('\n', iters, 'iterations')
-pass
+n = 100
+m = 30
+test_range = 100
+best_first = 0
+best_second = 0
+# with open('table_%dx%d.tsv' % (m,n), mode='w') as f:
+with stdout as f:
+    f.write('Жадный\t')
+    f.write('Неувязки\t')
+    f.write('Муравей\t')
+    if n<30:
+        f.write('Балаш\t')
+    f.write('\n')
+    for i in range(test_range):
+        c = [randi(1, 10) for i in range(n)]
+        a = [[]] * m
+        for i in range(len(a)):
+            a[i] = [randi(1, 10) for i in range(n)]
+        b = [sum(_a) / 3 for _a in a]
+        _, z1, _ = greed(c, a, b)
+        f.write('%d\t' % z1)
+        _, z2, _ = mod(c, a, b)
+        f.write('%d\t' % z2)
+        _, z4, _ = ant(c, a, b)
+        f.write('%d\t' % z4)
+        if n<30:
+            _, z5, _ = balas(c, a, b)
+            f.write('%d\t' % z5)
+        f.write('\n')
+        f.flush()
